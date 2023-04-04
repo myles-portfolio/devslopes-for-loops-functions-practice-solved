29d339dfbc7866242a4d1241b1781bf0c6c7f752
mylesh <84793716+myles-portfolio@users.noreply.github.com>
2023-04-04 09:15:57 -0500

    started e5

--- exercises/e5.js
+++ exercises/e5.js
@@ -4,12 +4,13 @@
 // Array example: bankAccounts in /data/data.js
 // getAllClientNames(bankAccounts) => ['Kevin', 'Spenser']
 
+import { bankAccounts } from "../data/data";
+
 export function getAllClientNames(array) {
-  // Your code goes here...
+  let arr = [];
+  for (let i = 0; i < [bankAccounts]; i++) {}
 
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-5"
