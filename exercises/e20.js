df5c44df792135521468349c3fb73cb14578e191
mylesh <84793716+myles-portfolio@users.noreply.github.com>
2023-04-06 08:43:19 -0500

    completed e20

--- exercises/e20.js
+++ exercises/e20.js
@@ -1,13 +1,32 @@
 
 // EXERCISE 20
-// Return and array of 2 arrays
+// Return an array of 2 arrays
 // (1 - an array of names that contain "a" in name, 2 - an array of names that don't have 'a' in name)
 // separateNamesWithAFromRest(["jon", "alice", "andrew", "mark", "jimmy"]) -> [["alice", "andrew", "mark"], ["jon", "jimmy"]]
 // NOTE: You MUST use double/nested FOR loop to solve this exercise. The array.includes() method is NOT allowed.
 
 export function separateNamesWithAFromRest(array) {
-  // Your code goes here...
+  const arr1 = [];
+  const arr2 = [];
 
+  for (let i = 0; i < array.length; i++) {
+    let name = array[i];
+    let letter = 'a';
+    let hasLetter = false;
+    
+    for (let n = 0; n < name.length; n++) {
+    if (name.charAt(n) === letter) {
+        hasLetter = true;
+        break;
+      }
+    }
+    if (hasLetter) {
+      arr1.push(array[i]);
+    } else {
+        arr2.push(array[i]);
+    }
+  }
+  return [arr1, arr2];
 }
 
 
