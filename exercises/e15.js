b9e636a0efa197953d3c081f0ee12282d791a116
mylesh <84793716+myles-portfolio@users.noreply.github.com>
2023-04-05 20:12:17 -0500

    completed e15

--- exercises/e15.js
+++ exercises/e15.js
@@ -5,8 +5,17 @@
 // NOTE: You can NOT use the array.concat() method in your code
 
 export function concatArrays(arr1, arr2) {
-  // Your code goes here...
+  let arr = [];
+  
+  for (let i = 0; i < arr1.length; i++) {
+    arr.push(arr1[i])
+  }
 
+  for (let y = 0; y < arr2.length; y++) {
+    arr.push(arr2[y])
+  }
+
+  return arr;
 }
 
 
